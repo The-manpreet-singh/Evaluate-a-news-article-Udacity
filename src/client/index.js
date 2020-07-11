869c6a972f515ee8a8738914fc06ea04df84abcd
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-11 22:43:17 +0530

    add index.js

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import { checkForName } from "./js/nameChecker";
+import { handleSubmit } from "./js/formHandler";
+
+// console.log(checkForName);
+
+// alert("I EXIST")
+// console.log("CHANGE!!");
+
+export { checkForName, handleSubmit };
