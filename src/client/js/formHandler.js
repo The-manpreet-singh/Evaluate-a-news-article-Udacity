db69032bfef8ac9032da01f0454086e26976cfda
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-13 21:34:56 +0530

    remoe commit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,12 +7,6 @@ function handleSubmit(event) {
 
 	console.log("::: Form Submitted :::");
 
-	// fetch('http://localhost:8080/test')
-	// .then(res => res.json())
-	// .then(function(res) {
-	//     document.getElementById('results').innerHTML = res.message
-	// })
-
 	let data = {
 		theText: formText,
 	};
