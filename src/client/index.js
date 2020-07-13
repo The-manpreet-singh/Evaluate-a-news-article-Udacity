503231b0a0b4b11363623da2f1a4a6fcdd1269e6
The-manpreet-singn <manpreet.singh10196@gmail.com>
2020-07-13 21:35:42 +0530

    remoe commit

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,6 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-// console.log(checkForName);
-
-// alert("I EXIST")
-// console.log("CHANGE!!");
-
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/form.scss'
